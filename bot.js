8df586252f42eb4f4d03fd51ec1b6f5a00e9da1c
FARES231 <51484866+FARES231@users.noreply.github.com>
2019-06-07 18:47:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -42,7 +42,7 @@ if (message.content.startsWith(prefix + 'setavatar')) {
 if (message.content.startsWith(prefix + 'setT')) {
   client.user.setGame(argresult, "https://www.twitch.tv/peery13");
      console.log('test' + argresult);
-    message.channel.sendMessage(`Streaming: **${argresult}**`)
+    message.channel.sendMessage(`**Streaming: **${argresult}**`)
 } 
 if (message.content.startsWith(prefix + 'setgame')) {
   client.user.setGame(argresult);
